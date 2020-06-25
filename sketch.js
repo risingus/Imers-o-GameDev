b28b59a02902ed31380917980eb83823de0f708c
Gustavo Lima <gustavo.it@outlook.com>
2020-06-25 19:16:54 -0300

    1. Adicionado inimigo na tela. 2. Adiconado animação do inimigo. 3. Adicionado movimentação do inimigo na fase

--- sketch.js
+++ sketch.js
@@ -1,13 +1,48 @@
 
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let cenario;
 let somDoJogo;
 let personagem;
+let inimigo;
+
+
+const matrizInimigo = [
+  [0, 0],
+  [105, 0],
+  [210, 0],
+  [315, 0],
+  [0, 104],
+  [105, 104],
+  [210, 104],
+  [315, 104],
+  [0, 208],
+  [105, 208],
+  [210, 208],
+  [315, 208],
+  [0, 312],
+  [105, 312],
+  [210, 312],
+  [315, 312],
+  [0, 409],
+  [105, 409],
+  [210, 409],
+  [315, 409],
+  [0, 503],
+  [105, 503],
+  [210, 503],
+  [315, 503],
+  [0, 609],
+  [105, 609],
+  [210, 609],
+  [315, 609],
+]
 
 function preload() {
   imagemCenario = loadImage('./imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('./imagens/personagem/correndo.png');
+  imagemInimigo = loadImage('./imagens/inimigos/gotinha.png')
   somDoJogo = loadSound('./sons/trilha_jogo.mp3');
 }
 
@@ -15,16 +50,19 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
   personagem = new Personagem(imagemPersonagem);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width -52, 52, 52, 104, 95);
   somDoJogo.loop();
-  frameRate(40)
-    
-  }
-  
-  function draw() {
-    cenario.exibe();
-    cenario.move();
-    personagem.exibe();
-       
-  }
+  frameRate(35)
 
+}
+
+function draw() {
+  cenario.exibe();
+  cenario.move();
+  personagem.exibe();
+  inimigo.exibe();
+  inimigo.move();
   
+
+}
+
